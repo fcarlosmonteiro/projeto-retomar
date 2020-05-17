00dda695f4db31efe2d9666302e075f64879e936
JailsonPanizzon <jailson.panizzon@gmail.com>
2020-05-17 00:27:19 -0300

    Normaliza horarios

--- src/utils/entradas.py
+++ src/utils/entradas.py
@@ -138,4 +138,19 @@ class Grafo(object):
 
     def __getitem__(self, v):
         return self.adj[v]
-ent = Entrada()
+
+def addTypes():
+    full_result = []
+    for i in range(0,4):
+        with open("../results/Quadrante_"+str(i)+".json", "r", encoding='utf-8') as resultado_file:
+            data_res = json.load(resultado_file)
+            for data in data_res:
+                with open('nearby.json', 'r', encoding='utf-8') as json_file:
+                    data_entrada = json.load(json_file)
+                    for places in data_entrada["results"]:
+                        if data["storeId"] == places["place_id"]:
+                            data["types"] = places["types"]
+                            full_result.append(data)
+                            break
+    with open('schedule.json', 'w', encoding='utf-8') as json_file:
+        data = json.dump(full_result, json_file)
